9892e3cde1023172d572abe5e98a2f4c1be5268f
yneskladovae <yneskladovae27@gmail.com>
2023-04-17 01:04:57 +0300

    useEffect, setTimeout, setInterval, clock

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -24,12 +24,64 @@ export const Ex1 = () => {
         console.log("useEffect first render and every counter change")
         document.title = counter.toString()
     }, [counter])
-    
+
     return (
         <>
             Hello, {counter} {fake}
             <button onClick={() => setFake(fake + 1)}>fake +</button>
             <button onClick={() => setCounter(counter + 1)}>counter +</button>
+        </>
+    )
+}
+
+export const SetTimeOutEx = () => {
+    console.log("SetTimeOutEx")
+    const [counter, setCounter] = useState(1);
+    const [counter2, setCounter2] = useState(1);
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log("setTimeout")
+            document.title = counter.toString()
+        }, 1000)
+    }, [counter])
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log("setTimeout")
+            setCounter2(state => state + 1)
+        }, 1000)
+    }, [])
+
+    return (
+        <>
+            Count num: {counter2}
+            <button onClick={() => setCounter(counter + 1)}>counter for document title</button>
+        </>
+    )
+}
+
+export const Clock = () => {
+    const [clock, setClock] = useState(new Date());
+    const hours = clock.getHours()
+    const minutes = clock.getMinutes()
+    const seconds = clock.getSeconds()
+
+    useEffect(() => {
+        const timing = setInterval(() => {
+            console.log("setTimeout")
+            setClock(new Date())
+        }, 1000)
+        return () => {
+            clearInterval(timing)
+        }
+    }, [clock])
+
+    return (
+        <>
+            <span>{hours}:</span>
+            <span>{minutes}:</span>
+            <span>{seconds}</span>
         </>
     )
 }
